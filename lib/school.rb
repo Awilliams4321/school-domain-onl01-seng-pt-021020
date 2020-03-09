c8c2524543a2f65fb17f8764f5240be68cdf189e
Amber Williams <awilliams1234@live.com>
2020-03-09 02:44:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School 
+  attr_accessor :roster, :student, :grade 
+  attr_reader :school 
+  
+  def initialize(school)
+    @school = school 
+  end 
+  
+  def roster
+    roster = {} 
+  end 
+  
+  def add_student(student, grade)
+    roster[grade] = {} 
+    roster[grade] << student  
+  end 
+end 
+ 
+  		   
